76790ded1234338c494020a6cb823a149cba5a80
Alice-C-C <alicechimim@gmail.com>
2024-06-27 11:34:17 -0300

    agora foi

--- potencia.js
+++ potencia.js
@@ -2,10 +2,20 @@ const prompt= require("prompt-sync")({sigint: true});
 
 let base= Number(prompt("Digite uma base: "))
 let expoente = Number(prompt('Digite um expoente: '))
-let resultado =0
+let resultado =base
+    
+    if(expoente ===0){
+        console.log("O resultado de todo expoente em zero é 1")
+        return
+    }
 
-for (let i = 0; i< expoente; i++) {
-    resultado += base
-   
+for (let i = 1; i< expoente; i++) {
+    let base_aux = resultado
+    console .log('base'+base_aux)
+    for (let j = 1; j < base; j++) {
+       resultado = resultado + base_aux;
+       console.log(resultado)
+    }
 }
 console.log(resultado)
+
